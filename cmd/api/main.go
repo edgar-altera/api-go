c23b225bda1b746fd4b8c4099edcc195505c8f8e
Edgar Parra <edgar@altera.cl>
2021-09-29 10:56:56 -0300

    Add routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/edgar-altera/api-go/internal/server"
+)
 
 func main() {
 
 	fmt.Println("Init App")
+	
+	r := server.NewRouter()
+ 
+	http.ListenAndServe(":8080", r)
 }
